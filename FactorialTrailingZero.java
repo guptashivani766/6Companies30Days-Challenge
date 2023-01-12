f10657958f4c881eaff0c39eb0129f679f01adc1
shivani gupta <shivanigupta4673@gmail.com>
2023-01-12 21:48:24 +0530

    Problem18

---
+++ FactorialTrailingZero.java
@@ -0,0 +1,11 @@
+class FactorialTrailingZero 
+{
+	  public int trailingZeroes(int n) {
+        int r = 0;
+        while (n > 0) {
+            n /= 5;
+            r += n;
+        }
+        return r;
+    }
+}
