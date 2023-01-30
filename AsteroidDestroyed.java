29664a9a28f2660a0c090f8297049964eda132a3
shivani gupta <shivanigupta4673@gmail.com>
2023-01-30 13:56:10 +0530

    Problem81

---
+++ AsteroidDestroyed.java
@@ -0,0 +1,14 @@
+class AsteroidDestroyed 
+{
+	public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        long m = mass;
+        Arrays.sort(asteroids);
+        for (int ast : asteroids) {
+            if (m < ast) {
+                return false;
+            }
+            m += ast;
+        }
+        return true;
+    }
+}
